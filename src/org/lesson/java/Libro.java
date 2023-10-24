8b5fead6cca6c8917b7e03e95616dabec38f02bd
DanieleCorrado <corrado_daniele@virgilio.it>
2023-10-24 14:56:08 +0200

    add Libro & Main

--- src/org/lesson/java/Libro.java
+++ src/org/lesson/java/Libro.java
@@ -1,2 +1,95 @@
-package org.lesson.java;public class Libro {
+package org.lesson.java;
+
+public class Libro {
+
+    String title;
+    int numberOfPage;
+    String author;
+    String publisher;
+
+    // COSTRUTTORI
+    public Libro(String title, int numberOfPage, String author, String publisher) throws IllegalArgumentException{
+
+        validateTitle(title);
+        validatePageNumber(numberOfPage);
+        validateAuthorName(author);
+        validatePublisher(publisher);
+        this.title = title;
+        this.numberOfPage = numberOfPage;
+        this.author = author;
+        this.publisher = publisher;
+    }
+
+    // METODI
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        validateTitle(title);
+        this.title = title;
+    }
+
+    public int getNumberOfPage() {
+        return numberOfPage;
+    }
+
+    public void setNumberOfPage(int numberOfPage) {
+        validatePageNumber(numberOfPage);
+        numberOfPage = numberOfPage;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        validateAuthorName(author);
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        validatePublisher(publisher);
+        this.publisher = publisher;
+    }
+
+    // Validatori
+    private void validateTitle(String title) {
+        if(title.isEmpty()) {
+            throw new IllegalArgumentException("The title's name cannot be empty");
+        }
+    }
+
+    private void validatePageNumber(int numberOfPage) {
+        if(numberOfPage <= 0) {
+            throw new IllegalArgumentException("The number of pages must be greater than 0");
+        }
+    }
+
+    private void validateAuthorName(String author) {
+        if(author.isEmpty()) {
+            throw new IllegalArgumentException("The author's name cannot be empty");
+        }
+    }
+
+    private void validatePublisher(String publisher) {
+        if(publisher.isEmpty()) {
+            throw new IllegalArgumentException("The publisher's name cannot be empty");
+        }
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "title:" + title +
+                ", number of Page: " + numberOfPage +
+                ", author: " + author  +
+                ", publisher: " + publisher +
+                '}';
+    }
 }
--- src/org/lesson/java/Main.java
+++ src/org/lesson/java/Main.java
@@ -1,2 +1,46 @@
-package org.lesson.java;public class Main {
+package org.lesson.java;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many books do you want to add? ");
+        int booksNumber = Integer.parseInt(scanner.nextLine());
+        Libro[] books = new Libro[booksNumber];
+
+        for (int i = 0; i < booksNumber; i++) {
+
+            System.out.print("Enter the title of the book: ");
+            String name = scanner.nextLine();
+
+            // controllo se il valore inserito è un intero
+
+            int bookPages = 0;
+
+            try {
+                System.out.print("Enter the number of pages: ");
+                bookPages = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                throw new NumberFormatException("Invalid number!");
+            }
+
+            System.out.print("Enter the author's name: ");
+            String author = scanner.nextLine();
+
+            System.out.print("Enter the publisher's name: ");
+            String publisher = scanner.nextLine();
+
+            books[i] = new Libro(name, bookPages,author,publisher);
+        }
+
+        for (int i = 0; i < booksNumber; i++) {
+            System.out.println(books[i]);
+        }
+
+        scanner.close();
+    }
 }
